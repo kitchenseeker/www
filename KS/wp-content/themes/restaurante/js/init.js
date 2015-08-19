f90a910fba1d4dc98b0b94fe4b803f15b7840ae5
Cloud Architect <gianluka@appnode-test.c.augmented-form-103908.internal>
2015-08-19 09:36:53 +0000

    first dev commit

--- KS/wp-content/themes/restaurante/js/init.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function($){
-$('.main-menu').slicknav({
-    label:init_vars.label
- });
-});
--- KS/wp-content/themes/restaurante/sidebar.php
+++
@@ -1,8 +0,0 @@
-		  <div class="col-md-4">
-			<?php if ( ! dynamic_sidebar( 'sidebar' ) ) : ?>
-			<div class="pre-widget">
-				<h3><?php _e('Widgetized Sidebar', 'restaurante'); ?></h3>
-				<p><?php _e('This panel is active and ready for you to add some widgets via the WP Admin', 'restaurante'); ?></p>
-			</div>
-			<?php endif; ?>
-		  </div>
